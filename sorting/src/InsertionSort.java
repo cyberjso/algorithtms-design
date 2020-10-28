f680eebea3369c8ac8eba04f70321e57eb6c5e7a
Jackson Oliveira <jackson.oliveira.rs@gmail.com>
2020-10-27 23:24:30 -0300

    Optmization on insertion sorting

--- sorting/src/InsertionSort.java
+++ sorting/src/InsertionSort.java
@@ -2,47 +2,35 @@ import java.util.Arrays;
 
 /**
  * Split the list in two groups (sorted and unsorted). Sorted elements are the ones on the left. For each element on the
- * the unsorted partition (elements on the right), compares with element on the sorted partition, until the array
- * becomes sorted.
+ * the unsorted partition (elements on the right). Shift the elements from the unsorted partition to the sorted one until
+ * It is ordered.
  *
  * worst time complexity: O(n²)
  * in-place: yes (works on the same given array to order, does not require data structure be duplicated)
  * stable: yes (does not change order of elements with the same key)
- * Recommended: Small lists, simple implementation when compared to more complex algorithms (merge, quick sort)
+ * Recommended: Small lists, simple implementation when compared to more complex algorithms (merge, quick sort) or when most
+ * of the elements is already ordered.
  */
 public class InsertionSort {
 	
 	public static void main(String[] args) {
-		int[] elements = new int[] { -10, 100, 50, -30, 20, 17, 1000, -70 };
-		
-		int sortedPartition = 0;
-		for (int i = 1; i < elements.length; i++) {
-			
-			boolean notOrdered = true;
-			int iterations = 0;
-			while (notOrdered) {
-				int orderedElement  = elements[sortedPartition - iterations];
-				int currentElement  =  elements[i - iterations];
-				boolean isSwapped = false;
+		int[] elements = new int[] { 20, 35, -15, 7, 55, 1, -22 };
+		int loopCounter = 0;
+		for (int unsortedIndex = 1; unsortedIndex < elements.length; unsortedIndex++) {
+			int currentElement  = elements[unsortedIndex];
 
-				// Swap condition
-				if (orderedElement > currentElement) {
-					elements[sortedPartition - iterations] = currentElement;
-					elements[i - iterations] = orderedElement;
-					isSwapped =  true;
-				}
-
-				// Stop condition. Iteration reaching the end of the list or no elements exchanged positions.
-				if (!isSwapped || sortedPartition - iterations == 0) {
-					notOrdered = false;
-					sortedPartition = i;
-				}
-				
-				iterations++;
+			int index;
+			for (index =  unsortedIndex; index > 0 && elements[index - 1] > currentElement; index--) {
+				elements[index] =  elements[index - 1];
+				loopCounter++;
 			}
+			elements[index] =  currentElement;
+			loopCounter++;
 		}
-		
-		System.out.println(Arrays.toString(elements));
+
+
+		System.out.println("Ordered elements: " + Arrays.toString(elements));
+		System.out.println("Iterations: " + loopCounter);
 	}
 
 }
